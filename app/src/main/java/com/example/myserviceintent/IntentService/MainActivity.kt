8537e68a410b1a32ee7ad607f48ad49d13406782
Krystian Wiewior <krystian.wiewior@gmail.com>
2021-03-12 13:49:47 +0100

    IntentService Example

---
+++ app/src/main/java/com/example/myserviceintent/IntentService/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.myserviceintent.IntentService
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myserviceintent.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        btnStartService.setOnClickListener {
+            Intent(this, MyIntentService::class.java).also {
+            startService(it)
+            tvServiceInfo.text = "Service runnning"
+            }
+
+        }
+
+    btnStopService.setOnClickListener {
+        MyIntentService.stopService()
+        tvServiceInfo.text = "Service stopped"
+    }
+
+
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/myserviceintent/IntentService/MyIntentService.kt
@@ -0,0 +1,42 @@
+@file:Suppress("DEPRECATION")
+
+package com.example.myserviceintent.IntentService
+
+import android.app.IntentService
+import android.content.Intent
+import android.util.Log
+
+class MyIntentService : IntentService("MyIntentService"){
+
+    init {
+        instance = this
+    }
+
+
+    companion object {
+        private lateinit var instance: MyIntentService
+        var isRunning = false
+
+        fun stopService() {
+            Log.d("MyIntentService", "Service is stopping...")
+            isRunning = false
+            instance.stopSelf()
+        }
+    }
+
+
+    override fun onHandleIntent(intent: Intent?) {
+        try {
+            isRunning = true
+            while (isRunning == true) {
+                Log.d("MyIntentService", "Service is running")
+                Thread.sleep(1000)
+            }
+        } catch (e: InterruptedException) {
+            Thread.currentThread().interrupt()
+        }
+    }
+
+
+
+}
